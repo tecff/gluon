5261869b0efa1954fde110bd189aed32474d8387
Brother-Lal <Brother-Lal@users.noreply.github.com>
2016-03-13 16:58:54 +0100

    Raspberry Pi build targets: remove BROKEN flag

--- targets/targets.mk
+++ targets/targets.mk
@@ -5,10 +5,10 @@ $(eval $(call GluonTarget,x86,generic))
 $(eval $(call GluonTarget,x86,kvm_guest))
 $(eval $(call GluonTarget,x86,64))
 $(eval $(call GluonTarget,x86,xen_domu))
+$(eval $(call GluonTarget,brcm2708,bcm2708)) # Raspberry PI A/B/B+ - more testing needed
+$(eval $(call GluonTarget,brcm2708,bcm2709)) # Raspberry PI 2 - more testing needed
 
 ifneq ($(BROKEN),)
 $(eval $(call GluonTarget,ramips,rt305x)) # BROKEN: No AP+IBSS support
-$(eval $(call GluonTarget,brcm2708,bcm2708)) # BROKEN: Needs more testing
-$(eval $(call GluonTarget,brcm2708,bcm2709)) # BROKEN: Needs more testing
 $(eval $(call GluonTarget,sunxi)) # BROKEN: Untested, no sysupgrade support
 endif
