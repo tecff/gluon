60c5b42989ac09268b51439475e29561983d3f2a
Andreas Ziegler <github@andreas-ziegler.de>
2016-09-07 21:42:39 +0200

    gluon-config-mode-hostname: add more detailed description

--- package/gluon-config-mode-hostname/luasrc/lib/gluon/config-mode/wizard/0100-hostname.lua
+++ package/gluon-config-mode-hostname/luasrc/lib/gluon/config-mode/wizard/0100-hostname.lua
@@ -1,7 +1,17 @@
 return function(form, uci)
 	local pretty_hostname = require "pretty_hostname"
 
-	local s = form:section(Section)
+	local msg = translate(
+		'Your node should have an informative and nice name. ' ..
+		'The node will be shown on the map using this name. ' ..
+		'You may use its location (e.g. "Placeholderstreet 5", "Townname 9"), ' ..
+		'the companies name (e.g. "Some Company") ' ..
+		'or similar. If there are multiple nodes in the same building, ' ..
+		'identifying them is easier if you put something like "2nd floor" ' ..
+		'at the end of the node\'s name.'
+	)
+
+	local s = form:section(Section, nil, msg)
 	local o = s:option(Value, "hostname", translate("Node name"))
 	o.default = pretty_hostname.get(uci)
 
